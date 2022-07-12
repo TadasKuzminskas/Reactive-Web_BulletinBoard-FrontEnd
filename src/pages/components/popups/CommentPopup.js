7662e34d9cd67f0737571a890983d57c1b1aab40
Dominykas Bartkus <dominykasbart@gmail.com>
2022-07-12 17:39:46 +0300

    Attempted to resolve calling undefined method problem

--- src/pages/components/popups/CommentPopup.js
+++ src/pages/components/popups/CommentPopup.js
@@ -1,63 +1,75 @@
 import axios from "axios";
-import React from "react";
-import "./Popup.css"
-import { useState } from 'react';
-import Comments from "../Comments";
+import React, { useRef } from "react";
+import "./Popup.css";
 
-function CommentPopup({updateComments, ...props}) {
+const CommentPopup = ({ updateComments, ...props }) => {
+  // This way you don't need to update state on each input change
+  const commentInputRef = useRef(null);
+  const postComment = async (comment) => {
+    try {
+      const commentToPost = {
+        content: comment,
+        username: props.activeUser,
+        post: props.postId,
+      };
+      const response = await axios.post(
+        "http://localhost:9090/v1/comment",
+        commentToPost
+      );
 
-    const [comment, setComment] = useState("")
-    
+      commentToPost.id = response.data;
 
-    // const handleCommentChange = (event) => {
-    // updateCommentsFunction = updateComments;
-    //     setComment(event.target.value);
-    //   }
-    
-
-    // console.log("Received updateComments function: ", updateComments)
-
-    const postComment = async () => {
-        try {
-          const commentToPost = {
-            content: comment,
-            username: props.activeUser,
-            post: props.postId,
-            id: 0
-          };
-          console.log('Received function before axios:', updateComments);
-          const response = await axios.post("http://localhost:9090/v1/comment", commentToPost);
-    
-          props.setTrigger(false);
-    
-          commentToPost.id = response.data;
-    
-          console.log('Received function after async:', updateComments);
-          
-          updateComments(commentToPost);
-          
-        } catch (error) {
-          console.error(error);
-        }
+      updateComments(commentToPost);
+      // Without the "?"" it will throw an error. As you are trying to invoke a method that is not passed to this component
+      // from the Comments.js component.
+      // This will crash the component and every other prop becomes undefined. So perhaps this was the cause of your "undefined" problem.
+      props?.setTrigger(false);
+    } catch (error) {
+      console.error(error);
     }
+  };
+  const submitForm = (event) => {
+    event.preventDefault();
 
+    // This is your current value of the textarea input.
+    // This way you avoid needless component re-rendering when updating state on each input.
+    // I do not know why the comopnent loses props when re-rendering after change.
+    // But this generally should solve the problem.
+    postComment(commentInputRef.current.value);
+  };
 
-    return ( props.trigger) ? (
-         <div className="popup">
-            <div className="popup-inner">
-            <button className="close-btn" onClick={() => props.setTrigger(false)}>X</button>
-                <form>
-                    <label>
-                        Comment:
-                        <textarea cols={20} rows={5} maxLength={255} onChange={event => setComment(event.target.value)}/>
-                    </label>
-                </form>
-                <button className="confirm" class="button" onClick={postComment}>comment</button>
-                
-            </div>
-        </div>
-    ) : "";
-}
+  // It's way cleaner to return early from a condition than to use
+  // shorthand conditional in this situation.
+  // The previous method made the code unreadable.
+  if (!props.trigger) {
+    return null;
+  }
 
+  return (
+    <div className="popup">
+      <div className="popup-inner">
+        <button className="close-btn" onClick={() => props?.setTrigger(false)}>
+          X
+        </button>
+
+        <form onSubmit={submitForm}>
+          <label>
+            Comment:
+            <textarea
+              ref={commentInputRef}
+              cols={20}
+              rows={5}
+              maxLength={255}
+            />
+          </label>
+        </form>
+
+        <button className="confirm" type="submit" onClick={postComment}>
+          Comment
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default CommentPopup;
